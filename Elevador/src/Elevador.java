3851beb590b7583e7b16a0dee9481d94f6b335c1
EduardoAnanias05 <edujujunior@icloud.com>
2024-02-16 13:23:00 -0300

    Organizando

---
+++ Elevador/src/Elevador.java
@@ -0,0 +1,28 @@
+/**
+ * Elevador
+ */
+public class Elevador {
+    // atributos
+    private int andarAtual;
+
+    // metodos
+
+
+    //contrutor
+    public Elevador() {
+        this.andarAtual = 0;
+
+    }
+
+   
+
+    // get and set  
+    
+    public int getAndarAtual() {
+        return andarAtual;
+    }
+
+    public void setAndarAtual(int andarAtual) {
+        this.andarAtual = andarAtual;
+    } 
+}
